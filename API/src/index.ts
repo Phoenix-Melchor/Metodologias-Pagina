9984737db60328397fdadaaf065155faabe85577
Phoenix-Melchor <149030452+Phoenix-Melchor@users.noreply.github.com>
2023-12-15 17:10:17 -0600

    Update index.ts

--- API/src/index.ts
+++ API/src/index.ts
@@ -12,7 +12,7 @@ app.use(cors())
 dotenv.config({});
 
 // Start the application by listening to specific port
-const port = Number(process.env.PORT || process.env.PORT || 8080);
+const port = Number(process.env.PORT || 8080);
 
 app.listen(port, () => {
   console.info('Express application started on port: ' + port);
